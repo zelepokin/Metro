2270cdf323ad3f19682bc949d9c7067dfa286702
zelepokin <64496527+zelepokin@users.noreply.github.com>
2020-05-12 22:51:10 +0300

    Add files via upload

---
+++ Metro/Form1.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.B1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'B1
+        '
+        Me.B1.Location = New System.Drawing.Point(320, 259)
+        Me.B1.Name = "B1"
+        Me.B1.Size = New System.Drawing.Size(160, 91)
+        Me.B1.TabIndex = 0
+        Me.B1.Text = "Обчислити"
+        Me.B1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.B1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents B1 As Button
+End Class
---
+++ Metro/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub B1_Click(sender As Object, e As EventArgs) Handles B1.Click
+        Dim N As Long, G As Integer, p As Single
+        N = 7000000
+        G = 2000
+        p = 7
+        Do While N < 10000000
+            N = N + N / 100 * p
+            G = G + 1
+        Loop
+        MsgBox(CStr(G) + "год")
+    End Sub
+End Class
